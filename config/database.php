5dd11a654c3f516967170af1e283a125f876dd6a
GRAEFFs2 <ggraeff@gmail.com>
2025-04-06 12:10:46 -0300

    Initial commit

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'agendamento_medico',
+    'username' => 'root',
+    'password' => '', // Senha padrão do XAMPP é vazia
+    'charset' => 'utf8mb4'
+]; 
---
+++ src/Database/Connection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database;
+
+class Connection {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        $config = require __DIR__ . '/../../config/database.php';
+        
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['database']};charset={$config['charset']}";
+        
+        try {
+            $this->connection = new \PDO($dsn, $config['username'], $config['password']);
+            $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+            $this->connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (\PDOException $e) {
+            throw new \Exception("Erro na conexão com o banco de dados: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+} 
